4fd7cbf065420ca80c224ba996c92b00db253567
Jcousar12 <158200825+Jcousar12@users.noreply.github.com>
2024-02-06 17:52:20 +0000

    daily assignment

---
+++ activityFeb6.py
@@ -0,0 +1,22 @@
+#: What is complexity?
+# A: Is the measure of resources a program uses
+# Resource =time,data,energy.
+
+# Q: What is abstraction?
+# A: The process of simplifying or hiding complexity.
+
+# What is a Variable?
+# A: A container for data
+
+# Rules of naming variables
+# 1. Variables name are short and descriptive.
+# 2. Variables names can start with a letters
+# or an underscore (_)
+
+# 4. Variables Cannot be Python keywords
+
+name='IAN
+payment= 100'
+
+x=100 
+x-=3
