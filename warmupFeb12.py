c884ee43bb39229e54e414912caa610aa3de63cc
Jcousar12 <158200825+Jcousar12@users.noreply.github.com>
2024-02-12 16:39:24 +0000

    warmup

---
+++ warmupFeb12.py
@@ -0,0 +1,4 @@
+"strings and numbers and it would be a string of 20"
+"float due to the decimal"
+"when data types does not match and type error can come up"
+"the computer does not understand the text"
