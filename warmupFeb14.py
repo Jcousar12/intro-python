f39735c32a207393bf03327f3e50c800be4560d0
Jcousar12 <158200825+Jcousar12@users.noreply.github.com>
2024-02-20 17:29:12 +0000

    jhazeer cousar quiz.py

---
+++ warmupFeb14.py
@@ -0,0 +1,4 @@
+"A built in function is an function that is already available in programming language"
+"A built in function that we used in class was print input"
+"A data type that can be assigned inside of a python " 
+groceryList=["cereal"]
