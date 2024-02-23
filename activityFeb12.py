e32df2b844d79cb0ac401f05b3bc263a88001d88
Jcousar12 <158200825+Jcousar12@users.noreply.github.com>
2024-02-23 17:13:14 +0000

    feb21_asyncWork.md

--- activityFeb12.py
+++ activityFeb12.py
@@ -1,2 +1,22 @@
-""
+# Two parts that are needed to create a function are function defintion and function call.
+# Function Defintion--> Defines the functions. Tells
+# the program how your function works.
+# Saves the instructions and runes when its called.
+# Goodmorning()# runs and executes the instructions.
 
+
+
+
+# A function parameter is the variable listed inside the parenthesis in the function defintion. 
+#An argument is the value that is sent to the function when its called. "
+def goodAfternoon(name):
+    print(f'good afternoon, {name}.'
+          
+# Clues--> 
+# argument-->name and age
+# print--> to send out data
+# function call + function definition
+# add 10--> Arithmatic
+
+def nameAndAge() :
+       print(f'')  
