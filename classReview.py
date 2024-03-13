5aef8154ee00d8d05aea94efe11514f741ca61c0
Jcousar12 <158200825+Jcousar12@users.noreply.github.com>
2024-03-13 15:50:47 +0000

    async_3_11.py 1

---
+++ classReview.py
@@ -0,0 +1,40 @@
+# python Review sheet
+
+# Vocabulary
+
+#. Program - Instructions for computers. 
+
+# 2. Programing language - Different ways we can write intructions for computers
+
+# 3. Syntax - the formatting rules of a language.
+# In this case, a programming 
+
+# This is how you write a function python
+def myFunction():
+    print('here are my instructions...')
+
+
+    # this is how you write a function in Javascript
+    function myFunction()
+    {console.log("here are my instructions...")
+     }
+    
+    # this is how you write a function in Java.
+    
+    public class myMethod{
+
+
+
+
+
+    # 4. Data type - the basic building
+        # Blocks for a programming language.
+        # basically our programming languages' alphabet
+
+
+    # 5. Variables - containers for data (Datatypes)
+        
+
+        # Additional Note - when we are writing programs,
+        # all we are doing is talking to the computer and giving 
+        # it instructions to complete a task.
